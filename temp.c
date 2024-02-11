893ef8f93a2ff2816d642a93ace1b04587139b8f
Aditya Mehrotra <adi1.mehrotra@gmail.com>
2024-02-11 03:11:20 -0600

    Update temp.c

--- temp.c
+++ temp.c
@@ -18,19 +18,4 @@ int main(int argc, char **argv) {
   } else {
     printf("Wrong Input");
   }
-
-  
-
-  lower = 0;
-  upper = 300;
-  step = 20;
-
-  fahr = lower;
-
-  printf("Fahrenheit \t  Celsius\n");
-  while (fahr <= upper) {
-    celsius = (5.0 / 9.0) * (fahr - 32.0);
-    printf("%3.0f \t\t %6.2f\n", fahr, celsius);
-    fahr += step;
-  }
 }
